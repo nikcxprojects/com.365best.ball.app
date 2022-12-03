5b01598d12c603093c7ee9bb0b39bbcb87202e53
Sladkiy Eduard <nikcxprojects@gmail.com>
2022-12-03 14:12:52 +0300

    update

--- Assets/com.penaltym.match/Scripts/Enums/Window.cs
+++
@@ -1,9 +0,0 @@
-public class Window
-{
-    public static string Menu { get => "Menu"; }
-    public static string Settings { get => "Settings"; }
-    public static string Balls { get => "Balls"; }
-    public static string Game { get => "Game"; }
-    public static string Pause { get => "Pause"; }
-    public static string GameOver { get => "Game Over"; }
-}
