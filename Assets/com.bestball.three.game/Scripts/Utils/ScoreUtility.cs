675afb4eadc7400b492799aa3c8e0b28ab51c2f3
Sladkiy Eduard <nikcxprojects@gmail.com>
2022-12-06 00:18:31 +0300

    update

--- Assets/com.bestball.three.game/Scripts/Utils/ScoreUtility.cs
+++ Assets/com.bestball.three.game/Scripts/Utils/ScoreUtility.cs
@@ -2,21 +2,21 @@ using UnityEngine;
 
 public static class ScoreUtility
 {
-    public static int CurrentScore 
-    { 
-        get => PlayerPrefs.GetInt("CurrentScore", 0); 
-        set => PlayerPrefs.SetInt("CurrentScore", value); 
+    public static int CurrentScore
+    {
+        get => PlayerPrefs.GetInt($"SCORE:{AppManager.CurrentGameType}", 0);
+        set => PlayerPrefs.SetInt($"SCORE:{AppManager.CurrentGameType}", value);
     }
 
     public static int BestScore
     {
-        get => PlayerPrefs.GetInt("BestScore", 0);
+        get => PlayerPrefs.GetInt($"SCORE:{AppManager.CurrentGameType}", 0);
 
         set
         {
             if(CurrentScore > BestScore)
             {
-                PlayerPrefs.SetInt("BestScore", CurrentScore);
+                PlayerPrefs.SetInt($"SCORE:{AppManager.CurrentGameType}", CurrentScore);
                 PlayerPrefs.Save();
             }
         }
