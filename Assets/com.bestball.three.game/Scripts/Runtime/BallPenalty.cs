da9296fe9822de8ae4c8bccd2c92956d72b0de5b
Sladkiy Eduard <nikcxprojects@gmail.com>
2022-12-05 15:49:43 +0300

    update

--- Assets/com.bestball.three.game/Scripts/Runtime/BallPenalty.cs
+++ Assets/com.bestball.three.game/Scripts/Runtime/BallPenalty.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class BallPenalty : MonoBehaviour
 {
-    private Vector2 initScale = Vector3.one * 0.3491603f;
-    private Vector2 targetScale = Vector3.one * 0.1604941f;
+    private Vector2 InitScale { get; set; } = Vector3.one * 0.3491603f;
+    private Vector2 TargetScale { get; set; } = Vector3.one * 0.1604941f;
 
     private Transform Center { get; set; }
+    private Transform Target { get; set; }
+
     private const float totalDistance = 4.0f;
 
     [SerializeField] float force;
@@ -17,17 +19,22 @@ public class BallPenalty : MonoBehaviour
     private void Start()
     {
         Center = GameObject.Find("center").transform;
+        Target = GameObject.Find("target").transform;
+
         Rigidbody = GetComponent<Rigidbody2D>();
     }
 
     private void OnMouseDown()
     {
-        Rigidbody.AddForce(Vector2.up * force, ForceMode2D.Impulse);
+        Target.localPosition = new Vector2(Random.Range(-2.34f, -2.34f), Random.Range(-0.65f, 1.35f));
+        Vector2 direction = Target.position - transform.position;
+
+        Rigidbody.AddForce(direction.normalized * force, ForceMode2D.Impulse);
     }
 
     private void Update()
     {
         float distanceToGoal = Mathf.Abs(Center.position.y - transform.position.y);
-        transform.localScale = Vector2.Lerp(targetScale, initScale, distanceToGoal / totalDistance);
+        transform.localScale = Vector2.Lerp(TargetScale, InitScale, distanceToGoal / totalDistance);
     }
 }
